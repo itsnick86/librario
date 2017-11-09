1118fa54bed3be8fc494ba60cee9e0c6a3a85532
Nick Edwards <nledwa01@gmail.com>
2017-11-08 20:22:53 -0500

    Installed NodeJS, Installed core packages, and set up the structure of the project.

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+// src/config/index.js
+
+module.exports = {
+  appName: 'Librario',
+  port: 3030
+}
---
+++ src/server.js
@@ -0,0 +1,14 @@
+// src/server.js
+
+const express = require('express');
+const config = require('./config');
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.end("Hello World!");
+});
+
+app.listen(config.port, function() {
+  console.log(`${config.appName} is listening on port ${config.port}`);
+});
